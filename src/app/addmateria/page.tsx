671337b62f14714422f1e99c7c4b0b8fd5142180
Gustavo Vinhola <gustavo.vinhola27@gmail.com>
2024-05-06 20:45:47 -0300

    Add materia/add task

---
+++ src/app/addmateria/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import AddSubjectScreen from "@/components/add";
+
+export default () => {
+    return (
+        <>
+        <Navbar></Navbar>
+        <AddSubjectScreen></AddSubjectScreen>
+        </>
+    )
+}
---
+++ src/app/addtask/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+import AddTaskScreen from "@/components/addtask";
+
+export default () => {
+    return (
+        <>
+        <Navbar></Navbar>
+        <AddTaskScreen></AddTaskScreen>
+        </>
+    )
+}
